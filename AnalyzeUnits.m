0b0c8edcb782d801665dc40b7166c6d52e1711ee
cllantz <30599977+cllantz@users.noreply.github.com>
2017-07-31 12:17:09 -0400

    Update AnalyzeUnits.m

--- AnalyzeUnits.m
+++ AnalyzeUnits.m
@@ -1,17 +1,31 @@
+%% Light reintroduction after dark exposure reactivates plasticity in adults via perisynaptic activation of MMP-9
+% This is the supplement for the manuscript for SU and VEP analysis. This uses input from Tucker Davis Tech, Sorting via OpenSorter.
+% For many portions of this to work you will need TuckerDavis OpenEx
+% Lab of Elizabeth Quinlan, by Crystal Lantz 
+%
+% This is the main code for running spike organization and analysis, within it calls several scripts
+%   1. PullSpikes - pulls the raw waveforms, sort codes, epocs (when the stimulus occured) and timestamps
+%   2. Sort1 - Organizes all units with designated sort code 1 into electrode number
+%   3. Sort2 - Organizes all units with designated sort code 2 into electrode number
+%   4. Then we check to make sure we have the correct number of trials, for this study it is 200, and sets the time stamps for start/end
+%   5. SUOrg - Organizes spikes into experimental trials, numbers trials for easier raster plotting
+%   6. SpikeNums - Removes movement artifacts, where units occur on all channels at the same time
+% Useful output are:
+%   1. AllSpike - Total number of spikes for each sort code by electrode number
+%   2. AvgUnit1 (or2) - Average waveform for each unit by electrode number and sort number
+
 clear all
 close all
 
 TT = actxcontrol('TTank.X');
 invoke(TT,'ConnectServer','Local','Me')
- e=TT.OpenTank('F:\Projects\Data\ScalingProject\BE58\BE585','R');
+e=TT.OpenTank('F:\Directory\Tank\','R');
 
 %% User input for Block infomation
 % block number to access
- block_str = 'Block-';
+block_str = 'Block-';
 Block = input('Block?','s');
-% just type the number, bruh
 if isempty(Block)
-    % Default speed of grating in cycles per second:
     Block= '1';
 end;
 Block = strcat(block_str, Block);
