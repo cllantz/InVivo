36c73c3b7b163b9d761c7a1bf628bf59da328f29
cllantz <30599977+cllantz@users.noreply.github.com>
2017-07-31 12:19:51 -0400

    Update Sort1.m

--- Sort1.m
+++ Sort1.m
@@ -7,16 +7,11 @@ amp=abs(allmax-allmin);
 logamp = amp>0.4e-04;
 logmax = allmax<3.50e-04;
 logmin = allmin>-3.50e-04;
-% logamp = amp>0.4e-04;
-% logampb = amp<2.4e-04;
-% logmax = allmax<1.0e-04;
-% logmin = allmin>-1.5e-04;
 
 
 tabletest=tabletest.*repmat(logmax,1,33);
 tabletest=tabletest.*repmat(logmin,1,33);
 tabletest=tabletest.*repmat(logamp,1,33);
-% tabletest=tabletest.*repmat(logampb,1,33);
 
 A = arrayfun(@(x) tabletest(tabletest(:,1) == x, :), unique(tabletest(:,1)), 'uniformoutput', false);
 
